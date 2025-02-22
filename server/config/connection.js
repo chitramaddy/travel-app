b3e5bf6b9e990a9cb2f6fe974ca7d58dc085b5ac
VPVelthuizen <vpvelthuizen@gmail.com>
2025-02-22 14:27:48 -0500

    more variables

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,17 @@ const mongoose = require('mongoose');
 
 // Connect to MongoDB using the connection URI from environment variables,
 // or fallback to a local MongoDB instance if the environment variable is not set.
-mongoose.connect(process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/nomadnotesdb');
+// mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/nomadnotesdb');
+
+// Use the environment variable for MongoDB connection
+const dbUrl = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/localDbName';
+
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 // Export the mongoose connection object to be used elsewhere in the application
 module.exports = mongoose.connection;
